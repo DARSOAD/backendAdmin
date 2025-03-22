1a2d106dc91ea3224c1a9349fa7ba133f38c9acb
Diego Rodríguez <dieguitotoi@gmail.com>
2025-03-22 12:53:55 -0500

    Entorno bien configurado

--- application.py
+++ application.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from users.routers import users
-from content.routers import blog
-
 app = FastAPI()
 
-# Registrar microservicios
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(blog.router, prefix="/content", tags=["Blog"])
+
 
 @app.get("/")
 def root():
